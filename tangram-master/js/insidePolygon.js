370f92edd0ff60e1ec63b83dd6eb7cf84cfd6e66
thomascastroneto <80851723+thomascastroneto@users.noreply.github.com>
2022-09-10 00:03:01 -0300

    Atualizações
    Implementação do algoritmo que calcula a área de um polígono e também de Sutherland, que retorna o polígono clipado.

---
+++ tangram-master/js/insidePolygon.js
@@ -0,0 +1,10 @@
+//Função que retorna se um ponto (pointx,pointy) está, ou não, no polígono poly
+//A variável inside que armazena esta informação
+function insidePoly(poly, pointx, pointy) {
+    var i, j;
+    var inside = false;
+    for (i = 0, j = poly.length - 1; i < poly.length; j = i++) {
+        if(((poly[i].y > pointy) != (poly[j].y > pointy)) && (pointx < (poly[j].x-poly[i].x) * (pointy-poly[i].y) / (poly[j].y-poly[i].y) + poly[i].x) ) inside = !inside;
+    }
+    return inside;
+}
